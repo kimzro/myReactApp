98dcdddb2dfee0e5b44ca258f6075847b9940a26
kimzro <vkzlzmffjq@gmail.com>
2018-07-19 11:36:00 +0900

    0719

---
+++ 0719/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import textBox from './textBox';
+
+class App extends Component {
+  /*componentWillmount(){
+
+  }*/
+
+  render() {
+    return (
+      <div className="App">
+        fakeittillyoumakeit        
+        <textBox/>
+      </div>
+    );
+  }
+
+  /*componentDidMount(){
+    
+  }*/
+}
+
+export default App;
